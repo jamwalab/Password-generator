3fbb773abc3091575d349ed5d3145276ad1ad8f2
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-05-05 02:34:36 -0400

    password length and input validation in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,66 @@
 // Assignment code here
-var passLength = function() {
-  var length = window.prompt("Please provide the length of the password. Minimum 8 characters and maximum 128.")
-  return length;
-}
 
+//-----FUNCTION TO DETERMINE PASSWORD LENGTH
+/*var passLength = function() {
+  var len = parseInt(window.prompt("Please provide the length of the password. Minimum 8 characters and maximum 128."));
+  console.log("Before if statements", len);
+  if (len > 128 || len < 8 ) {
+    window.alert("Invalid input!! Minimum password length is 8 and maximum 128.");
+    passLength();
+  }
+  else if (Number.isNaN(len)) {
+    window.alert("Invalid input!! Please enter a numeric value.");
+    passLength();
+  }
+  //else {
+    debugger;
+    console.log("After if statements", len);
+    return len;
+  //}
+}*/
+
+//-----OBJECT FOR PASSWORD DETAILS
 var passwordDetails = {
-  length: passLength(),
-}
+  length: 0,
+  lcase: true,
+  ucase: true,
+  num: true,
+  sChar: true,
 
-console.log(passwordDetails.length);
+  passLength: function() {
+    var len = parseInt(window.prompt("Please provide the length of the password. Minimum 8 characters and maximum 128."));
+    console.log("Before if statements", len);
+    if (len > 128 || len < 8 ) {
+      window.alert("Invalid input!! Minimum password length is 8 and maximum 128.");
+      this.passLength();
+    }
+    else if (Number.isNaN(len)) {
+      window.alert("Invalid input!! Please enter a numeric value.");
+      this.passLength();
+    }
+    else {
+      debugger;
+      this.length = len;
+    }
+  },
+
+  otherDetails: function() {
+    this.lcase = window.confirm("Do you want to include lower case letters?");
+    this.ucase = window.confirm("Do you want to include upper case letters?");
+    this.num = window.confirm("Do you want to include numbers?");
+    this.sChar = window.confirm("Do you want to include special characters?");
+    if (!(this.lcase || this.ucas || this.num || this.sChar)) {
+      window.alert("Please select at least one option from lowercase, uppercase, numbers or special characters. Please try again.");
+      this.otherDetails();
+    }
+  }
+};
+
+passwordDetails.passLength();
+passwordDetails.otherDetails();
+
+console.log(passwordDetails);
+console.log("Retuned value", passwordDetails.length);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
